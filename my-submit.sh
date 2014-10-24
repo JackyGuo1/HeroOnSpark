4ca29c6296b30c069697b30ad277b956e4ab21f7
JackyGuo1 <leon.k.scott@hotmail.com>
2014-10-24 11:40:53 +0000

    With topN in each group

--- my-submit.sh
+++ my-submit.sh
@@ -2,6 +2,10 @@ SPARK_HOME=/home/ansibler/work/spark/spark-1.1.0-bin-cdh4
 $SPARK_HOME/bin/spark-submit  \
   --class "com.zetdata.hero.trial.SimpleApp" \
    --master spark://10.10.0.114:7077 \
-   target/scala-2.10/spark_word_segement.jar 
+   --num-executors 3 --driver-memory 1024m  --executor-memory 1024m \
+   target/scala-2.10/spark_word_segement.jar \
+   hdfs://10.10.0.114/tmp/zetjob/admin/job320/blk1773/dump_dir \
+   hdfs://10.10.0.114/tmp/zetjob/admin/job320/blk1774/dump_dir \
+   hdfs://10.10.0.114/tmp/jiaqi-sbt/match_scoreSchema
 # --master spark://10.10.0.114:7077  \
  
--- src/main/scala/com/zetdata/hero/trial/SimpleApp.scala
+++ src/main/scala/com/zetdata/hero/trial/SimpleApp.scala
@@ -14,26 +14,28 @@ import java.util.LinkedList
 import java.util.Collections
 import java.util.Comparator
 import org.apache.spark.mllib.linalg.SparseVector
-
+import java.util.PriorityQueue
+import scala.collection.JavaConverters._
 
 object SimpleApp {
 
   def main(args: Array[String]) {
 
-    val rawFile = "/home/ansibler/jiaqi-sbt/simple/testData.txt"
-//    val jdFile = "/home/ansibler/jiaqi-sbt/simple/for-tfidf/jd.txt"
-    val jdFile = "hdfs://10.10.0.114/tmp/zetjob/admin/job320/blk1773/dump_dir"
-      
-      
-//    val rsFile = "/home/ansibler/jiaqi-sbt/simple/for-tfidf/rs.txt"
-    val rsFile = "hdfs://10.10.0.114/tmp/zetjob/admin/job320/blk1774/dump_dir"
+//        val jdFile = "hdfs://10.10.0.114/tmp/zetjob/admin/job320/blk1773/dump_dir"
+    val jdFile = args(0)
+
+  //      val rsFile = "hdfs://10.10.0.114/tmp/zetjob/admin/job320/blk1774/dump_dir"
+    val rsFile = args(1)
+
+ //    val match_result_dir = "hdfs://10.10.0.114/tmp/jiaqi-sbt/match_scoreSchema"
+    val match_result_dir = args(2)
+
     val conf = new SparkConf().setAppName("CosineMatch Application")
     val sc = new SparkContext(conf)
 
     val rs_documents: RDD[(Int, Seq[String])] = sc.textFile(rsFile).map(Utils.wordSegment)
-    
-    val jd_documents:RDD[(Int, Seq[String])] = sc.textFile(jdFile).map(Utils.wordSegment)
-    
+
+    val jd_documents: RDD[(Int, Seq[String])] = sc.textFile(jdFile).map(Utils.wordSegment)
 
     def cal_tfidf(documents: RDD[(Int, Seq[String])]): RDD[(Int, Array[Int], Array[Double])] = {
       val ids: RDD[Int] = documents.map((tuple: (Int, Seq[String])) => tuple._1)
@@ -54,16 +56,17 @@ object SimpleApp {
       ids.zip(descriptions.map(Utils.iterFunc))
     }
 
-    def cosine_match(tuple: ((Int, Array[Int], Array[Double]), (Int, Array[Int], Array[Double]))): ((Int, Int, Double),(Int,Int,List[(Int,Double)])) = {
+    def cosine_match(tuple: ((Int, Array[Int], Array[Double]), (Int, Array[Int], Array[Double]))): ((Int, Int, Double), (Int, Int, List[(Int, Double)])) = {
       var score: Double = 0.0
-      var match_item = List[(Int,Double)]()
+      var match_item = List[(Int, Double)]()
+
       var xi = 0
       var yi = 0
       while (xi < tuple._1._2.length && yi < tuple._2._2.length) {
 
         if (tuple._1._2(xi) == tuple._2._2(yi)) {
           match_item = match_item.+:((tuple._1._2(xi), Math.max(tuple._1._3(xi), tuple._2._3(yi))))
-          score = score + 1.0 //Math.max(tuple._1._3(xi), tuple._2._3(yi))
+          score = score + Math.max(tuple._1._3(xi), tuple._2._3(yi))
           xi = xi + 1
           yi = yi + 1
         } else if (tuple._1._2(xi) < tuple._2._2(yi)) {
@@ -73,35 +76,69 @@ object SimpleApp {
         }
 
       }
-      ((tuple._1._1,tuple._2._1,score/Math.sqrt(tuple._1._2.length*tuple._2._2.length)),(tuple._1._1,tuple._2._1,match_item))
+
+      ((tuple._1._1, tuple._2._1, score / Math.sqrt(tuple._1._2.length * tuple._2._2.length)), (tuple._1._1, tuple._2._1, match_item))
     }
 
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    import sqlContext.createSchemaRDD
-    case class MatchResult(id1: Int, id2: Int,score:Double)
-    
-    
+    def heapSelect(iter: Iterable[(Int, Int, Double)], K: Int) = {
+      val myComparator = new Comparator[(Int, Int, Double)]() {
+        def compare(x: ((Int, Int, Double)), y: ((Int, Int, Double))) = {
+          (x._3 - y._3).toInt
+        }
+      }
+      var heap = new PriorityQueue(10, myComparator)
+      for(i <- iter){
+         if(heap.size() < K || i._3  > heap.peek()._3){
+           if(heap.size() == K) heap.remove(heap.peek())
+           heap.offer(i)
+         }
+      }
+      var topList = new LinkedList[(Int,Int,Double)]()
+      while(!heap.isEmpty()){
+         topList.add(heap.poll())
+      }
+      topList.asScala
+    }
+
+    def topN(tuple: ((Int, Iterable[(Int, Int, Double)]))):List[(Int,Int,Double)] = {
+    	val topList = heapSelect(tuple._2, 10)
+        topList.toList
+    }
+
+//    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+//    import sqlContext.createSchemaRDD
+    case class MatchResult(id1: Int, id2: Int, score: Double)
+
     val rs_id_tfidf = cal_tfidf(rs_documents)
     val jd_id_tfidf = cal_tfidf(jd_documents)
 
     val rs_hashid = cal_hashid(rs_documents)
-    val jd_hashid = cal_hashid(jd_documents)    
+    val jd_hashid = cal_hashid(jd_documents)
+
     rs_id_tfidf.cache()
     jd_id_tfidf.cache()
     val match_result_analysis = rs_id_tfidf.cartesian(jd_id_tfidf).map(cosine_match)
+
+    val match_score_raw = match_result_analysis.map(_._1)
+
+    val match_score_candid_part = match_score_raw.filter(_._3 > 0.00001).groupBy(_._1)
+    match_score_candid_part.cache()
+    val match_score_candid_part_topN =match_score_candid_part.map(topN)
     
-    val match_score = match_result_analysis.map(_._1)
+    
+    
+    val match_score_candid_part_topN_flat = match_score_candid_part_topN.flatMap(_.iterator)
+    
+    val match_score_huntor_part = match_score_raw.filter(_._3 > 0.00001).groupBy(_._1)
+    match_score_huntor_part.cache()
+    val match_score_huntor_part_topN = match_score_huntor_part.map(topN)
+    val match_score_huntor_part_topN_flat = match_score_huntor_part_topN.flatMap(_.iterator)
+
+
     val match_analsis = match_result_analysis.map(_._2)
 
-    match_score.coalesce(1, false).map(tuple => "%s,%s,%s".format(tuple._1,tuple._2,tuple._3 )).saveAsTextFile("hdfs://10.10.0.114/tmp/jiaqi-sbt/match_scoreSchema")
-    
-/*    val match_score_schema = match_score.map(tuple => MatchResult(tuple._1,tuple._2,tuple._3))
-    match_score_schema.saveAsTextFile("./match_scoreSchema")*/
-    
-//    match_score.map((_._1,_._2,_._3))
-    
-/*    match_score.coalesce(1, false).saveAsTextFile("./match_score")
-    match_analsis.coalesce(1, false).saveAsTextFile("./match_analsis")*/
+    match_score_candid_part_topN_flat.++(match_score_huntor_part_topN_flat).coalesce(1, false).map(tuple => "%s,%s,%s".format(tuple._1, tuple._2, tuple._3)).saveAsTextFile(match_result_dir)
+
   }
 }
 
